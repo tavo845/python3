18ced941e9fe48f06a9a13e96d1bf242bc8d33a8
tavo845 <gustavoramirez8965@gmail.com>
2019-01-30 23:54:14 -0600

    no le entendi profe

---
+++ ejercicios.py
@@ -0,0 +1,13 @@
+##Gustavo Ramirez
+##crear un programa que permita sleccionar entre una de dos opciones 1.comvertir
+## dolares a quetzales o de quetzales a dolares si escoge una opcion invalida tenemos
+# que decirle "opcion incorrecta"
+print("Binvenido al programa".center(50,"*"))
+pdolar = float(input("Ingrese el precio del dolar:"))
+dolares = float(input("Ingrese la cantidad a dolares a comvertir:"))
+quetzales=pdolares * dolares
+print(dolares,"su cantidad es:{}",pdolar)
+#ercicio2.... solicitar al usuario que escoja una de las siguientes opciones
+#1.sumar 2 numeros 2. restar 3.numero 4.dividir 2 numeros si ingresa una opcion invalida
+#hacersela saber
+
